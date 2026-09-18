18fce6bc0b8528e38f17975b5b44a9bcbeb629c7
Benoit <benoitmbole4@gmail.com>
2026-09-18 09:13:41 +0200

    ip adress of ,env file added

--- test_server.py
+++ test_server.py
@@ -1,8 +1,13 @@
 import sys
+import os
+from dotenv import load_dotenv
 from mcstatus import JavaServer
 
-SERVER_ADDRESS = "127.0.0.1"
-SERVER_PORT = 8888
+# Load environment variables from .env file
+load_dotenv()
+
+SERVER_ADDRESS = os.getenv("MINECRAFT_SERVER_IP", "127.0.0.1")
+SERVER_PORT = int(os.getenv("MINECRAFT_SERVER_PORT", 8888))
 
 def test_minecraft_server(address, port):
     try:
